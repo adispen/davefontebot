bbb42ac32bb8bbe3d590368311f14b651f0e88b7
Aedan Dispenza <aedan.dispenza@gmail.com>
2014-01-19 06:04:15 -0500

    now refreshing in real time, tons of logging

--- fontebot.py
+++ fontebot.py
@@ -12,15 +12,22 @@ r = praw.Reddit(user_agent = user_agent)
 
 r.login('DaveFonteBot', PASSWORD)
 subreddit = r.get_subreddit('114g+ahf4')
-subreddit_comments = subreddit.get_comments()
 already_done = set()
+x = 1
 
 buzzWords = ['hackathon', 'hackathons', 'mhacks', 'hackru', 'pennapps', 'hackmit', 'hackny']
 while True:
+	subreddit_comments = subreddit.get_comments()
 	for comment in subreddit_comments:
 		comment_text = comment.body.lower()
 		has_buzz = any(string in comment_text for string in buzzWords)
+		x += 1
 		if comment.id not in already_done and has_buzz:
-			comment.reply("Helllllll yeah!")
+			print comment.author
+			print comment.body
+			print comment.id
+			print x
+			#comment.reply("Helllllll yeah!")
 			already_done.add(comment.id)
-	time.sleep(300)
+	print x
+	time.sleep(2)
